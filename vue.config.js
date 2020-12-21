8a62076a812c764fd65ceb0ebb32ea6a2c7f15c1
tristan.neumann <tristan.neumann@patientpop.com>
2020-12-20 22:50:43 -0800

    CNAME in deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  "publicPath": "/",
+  "publicPath": process.env.NODE_ENV === 'production'
+    ? '/weekness-for-food/'
+    : '/',
   "transpileDependencies": [
     "vuetify"
   ]
